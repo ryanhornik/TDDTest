f95a88fcc0940a7f8761e84324dbff693e392ad0
Ryan Hornik <ryanhornik@gmail.com>
2015-03-21 18:56:43 -0500

    Better unit testing practice

--- lists/tests.py
+++ lists/tests.py
@@ -9,6 +9,11 @@ from lists.views import home_page
 # Create your tests here.
 class HomePageTest(TestCase):
 
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
+
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
@@ -26,11 +31,12 @@ class HomePageTest(TestCase):
 
         response = home_page(request)
 
-        self.assertIn('A new list item', response.content.decode())
-        expected_html = render_to_string(
-            'home.html',
-            {'new_item_text': 'A new list item'})
-        self.assertEqual(response.content.decode(), expected_html)
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+        self.assertEqual(response.status_code, 302) # 302 = HTTP redirect
+        self.assertEqual(response['location'], '/')
 
 class ItemModelTest(TestCase):
 
