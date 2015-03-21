c9dfacbab82409d7566009260f17cc19b353c2fe
Ryan Hornik <ryanhornik@gmail.com>
2015-03-21 01:16:15 -0500

    Improved functional_tests, now fails when it can't find <h1>

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -17,17 +18,32 @@ class NewVisitorTest(unittest.TestCase):
 
         # user notices the page title and header mention To-Do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # User is invited to enter a to-do item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # User types "buy peacock feathers" into a textbox
+        inputbox.send_keys('Buy peacock feathers')
 
         # When the user hits enter the page updates and now the page list
         # "1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
         # There is another textbox for
         # enters "Use peacock feathers to make a fly"
+        self.fail('Finish the test')
 
         # Page updates and both items are on the list
 
