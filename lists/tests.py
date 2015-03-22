31767faa152ed8f2d4f4ab52beb977abedba8d5d
Ryan Hornik <ryanhornik@gmail.com>
2015-03-22 11:39:41 -0500

    List items save to database

--- lists/tests.py
+++ lists/tests.py
@@ -35,8 +35,25 @@ class HomePageTest(TestCase):
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
+    def test_home_page_redirects_after_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
         self.assertEqual(response.status_code, 302) # 302 = HTTP redirect
         self.assertEqual(response['location'], '/')
+
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
 
 class ItemModelTest(TestCase):
 
